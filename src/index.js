b22b85fd284d70c16e3bb45a0c12ec8cab38a4bf
ye-russell <bekberata@gmail.com>
2021-03-11 15:48:39 +0600

    fix: update index.js with correction

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function towelSort (matrix) {
   if (matrix == undefined) return [];
-  for (let i = 1; i < matrix.length; i + 2) {
+  for (let i = 1; i < matrix.length; i += 2) {
     matrix[i].reverse();
   }
   return matrix.flat();
